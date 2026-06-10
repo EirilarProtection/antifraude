7264f84fd7826134a1025defdf5c9ef98322c6da
EirilarProtection <hdse1994@gmail.com>
2026-06-10 20:56:33 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,14 +1,5 @@
-import psycopg2
-import os
-
 def get_connection():
-    db_url = os.getenv("postgresql://postgres:OfEYKNGiqqhUOhsNlwuYyxRzrMiiLsIj@acela.proxy.rlwy.net:22734/railway")
-
-    if not db_url:
-        raise Exception("DATABASE_URL não configurada")
-
     return psycopg2.connect(
-        db_url,
-        sslmode="require",
-        connect_timeout=10
+        "postgresql://postgres:OfEYKNGiqqhUOhsNlwuYyxRzrMiiLsIj@acela.proxy.rlwy.net:22734/railway",
+        sslmode="require"
     )
