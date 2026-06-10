319e13411e455e96ae92d43782e1cdf69a0f9fbb
EirilarProtection <hdse1994@gmail.com>
2026-06-10 18:46:49 -0300

    Update database.py

--- database.py
+++ database.py
@@ -1,6 +1,9 @@
 import psycopg2
 
+DATABASE_URL = "postgresql://postgres:OfEYKNGiqqhUOhsNlwuYyxRzrMiiLsIj@acela.proxy.rlwy.net:22734/railway"
+
 def get_connection():
     return psycopg2.connect(
-        "postgresql://postgres:OfEYKNGiqqhUOhsNlwuYyxRzrMiiLsIj@acela.proxy.rlwy.net:22734/railway"
+        DATABASE_URL,
+        sslmode="require"
     )
